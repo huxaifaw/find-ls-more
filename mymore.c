e47b8ef756f7edc95dbabca6e2d5d5d0ac060c23
Huzaifa Waseem <Huzaifa Waseem>
2019-03-17 20:22:57 +0500

    Open file in vim-Handle non canonical & non echo mode-Handle terminal size-%age of file contents displayed-I/O redirection handling-Reverse video feature-Reads and prints one page then pause for some special commands 'q', ' ', 'v', '\n'

--- mymore.c
+++ mymore.c
@@ -1,19 +1,24 @@
-//Handle size of terminal-Percentage of file contents displayed-I/O redirection handing-reverse video feature using control sequence introducer \033[-read and print one page then pause for a few special commands ('q', ' ' , '\n')
+//Open a file in vim-Handle non canonical and non echo mode-Handle size of terminal-Percentage of file contents displayed-I/O redirection handing-reverse video feature using control sequence introducer \033[-read and print one page then pause for a few special commands ('q', ' ' , '\n')
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
+#include <string.h>
 //#define PAGELEN 20
 #define	LINELEN	512
 int PAGELEN = 0;
-void do_more(FILE *);
+void do_more(FILE*);
+void mygrep(FILE*, char*);
 int get_input(FILE*, int, int);
 int get_total_num_of_lines(FILE*);
 void winch_handler(int);
 void set_input_mode(struct termios attr);
+char* filename[5];
 int main(int argc , char *argv[])
 {
 	struct termios attr, saved_attr;
@@ -35,6 +40,9 @@ int main(int argc , char *argv[])
         		perror("Can't open file");
          		exit (1);
       		}
+	filename[0] = "vim";
+	filename[1] = argv[i];
+	filename[2] = NULL;
       	do_more(fp);
       	fclose(fp);
    	}
@@ -64,8 +72,9 @@ int get_total_num_of_lines(FILE* fp)
 }
 void do_more(FILE *fp)
 {
-   	int num_of_lines = 0;
+	int num_of_lines = 0;
    	int rv, n = 0;
+	char str[20];
 	int total_noOfLines = get_total_num_of_lines(fp);
 	fseek(fp, 0, SEEK_SET);
    	char buffer[LINELEN];
@@ -75,8 +84,8 @@ void do_more(FILE *fp)
       		num_of_lines++;
       		n++;
 	      	if (num_of_lines == PAGELEN){
-		 	rv = get_input(fp_tty, n, total_noOfLines);		
-		 	if (rv == 0) {//user pressed q
+		 	rv = get_input(fp_tty, n, total_noOfLines);
+			if (rv == 0) {//user pressed q
 		    		printf("\033[2K \033[1G");
 		    		break;
 		 	}
@@ -92,6 +101,21 @@ void do_more(FILE *fp)
 		    		printf("\033[2K \033[1G");
 		    		break; 
 		 	}
+			else if(rv == 4) { //search a given string
+				printf("\033[1A \033[2K \033[1G");
+			}
+			else if(rv == 5) { //open file in vim editor
+				pid_t pid = fork();
+				if(!pid) {
+					execv("/bin/vim", filename);
+				}
+				else {
+					waitpid(pid, 0, 0);
+				}
+				printf("\033[1A \033[2K \033[1G");
+				printf("\033[1K \033[2G");
+				num_of_lines--;
+			}
 	      	}
 	}
 }
@@ -106,6 +130,32 @@ int get_input(FILE* cmdstream, int num_of_lines, int total_noOfLines)
       	if ( c == ' ' )			
 	 	return 1;
       	if ( c == '\n' )	
-	 	return 2;	
+	 	return 2;
+	if ( c == '/' )
+		return 4;
+	if ( c == 'v' )
+		return 5;	
       	return 3;
+}
+void mygrep(FILE* fp, char* str)
+{
+	char c1[100], c2[100];
+	while(fgets(c1, 100, fp))
+	{
+		char* temp;
+		int i = 0;
+		strncpy(c2, c1, 100);
+		temp = strtok(c1, " ");
+		while(temp)
+		{
+			if(!strcmp(temp, str))
+			{
+				fputs(c2, stdout);
+				printf("/n");
+				break;
+			}
+			temp = strtok(NULL, " ");
+
+		}
+	}
 }
