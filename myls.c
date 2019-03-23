ad8f63a0bfcc0553509be2637ce4cc6bb658baf3
Huzaifa Waseem <Huzaifa Waseem>
2019-03-23 17:31:02 +0500

    ls command v2

--- myls.c
+++ myls.c
@@ -5,9 +5,15 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <time.h>
+#include <pwd.h>
+#include <grp.h>
 
 extern int errno;
 void do_ls(char*);
+char* uidToUname(int);
+char* gidToGname(int);
+int cstring_cmp(const void*, const void*);
 int main(int argc, char* argv[])
 {
 	if(argc==1)
@@ -16,6 +22,7 @@ int main(int argc, char* argv[])
 		int i = 0;
 		while(++i < argc)
 		{
+			printf("Directory listing of &s:\n", argv[i]);
 			do_ls(argv[i]);
 		}
 	}
@@ -30,17 +37,69 @@ void do_ls(char* dir)
       	return;
    	}
    	errno = 0;
-   	while((entry = readdir(dp)) != NULL){
-        if(entry == NULL && errno != 0){
-  		perror("readdir failed");
-		exit(1);
+	int numOfNames = 0;
+   	while((entry = readdir(dp)) != NULL) {
+        	if(entry == NULL && errno != 0) {
+  			perror("readdir failed");
+			exit(errno);
+  		}
+		else {                
+			if(entry->d_name[0] != '.')
+        			numOfNames++;
+        	}     	
   	}
-	else
-	{                
-		if(entry->d_name[0] == '.')
-        	continue;
-        }     	
-	printf("%s\n",entry->d_name);
-   }
-   closedir(dp);
+	char** names;
+	names = (char**)malloc(sizeof(char**)*numOfNames);
+	rewinddir(dp);
+	int i = 0;
+	while((entry = readdir(dp)) != NULL) {
+		if(entry == NULL && errno != 0) {
+			perror("readdir failed");
+			exit(errno);
+		}
+		else {
+			if(entry->d_name[0] != '.')
+			{
+				names[i] = (char*)malloc(sizeof(char) * (strlen(entry->d_name) + 1));
+				strcpy(names[i], entry->d_name);
+				i++;
+			}
+		}
+	}
+	qsort(names, numOfNames, sizeof(char*), cstring_cmp);
+	for(int j = 0; j<numOfNames; j++)
+		printf("%s\n", names[j]);
+   	closedir(dp);
+}
+char* uidToUname(int uid)
+{
+	errno = 0;
+	struct passwd * pwd = getpwuid(uid);
+	if(pwd == NULL)
+	{
+		if(errno == 0)
+			return "Unknown User\n";
+		else
+			printf("getpwuid failed\n");
+	}
+	return pwd->pw_name;
+}
+char* gidToGname(int gid)
+{
+	errno = 0;
+	struct group * grp = getgrgid(gid);
+	if(grp == NULL)
+	{
+		if(errno == 0)
+			return "Unknown Group\n";
+		else
+			printf("getgrgid failed\n");
+	}
+	return grp->gr_name;
+}
+int cstring_cmp(const void* a, const void* b)
+{
+	const char **str1 = (const char **)a;
+	const char **str2 = (const char **)b;
+	return strcmp(*str1, *str2);
 }
