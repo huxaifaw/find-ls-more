f4631c9791208687f852467c5350259c84da0662
Huzaifa Waseem <Huzaifa Waseem>
2019-03-23 17:42:06 +0500

    ls command v3 with function of getting file types and permissions on a file

--- myls.c
+++ myls.c
@@ -14,6 +14,7 @@ void do_ls(char*);
 char* uidToUname(int);
 char* gidToGname(int);
 int cstring_cmp(const void*, const void*);
+char* typesAndPermissions(int);
 int main(int argc, char* argv[])
 {
 	if(argc==1)
@@ -102,4 +103,50 @@ int cstring_cmp(const void* a, const void* b)
 	const char **str1 = (const char **)a;
 	const char **str2 = (const char **)b;
 	return strcmp(*str1, *str2);
+}
+char* typesAndPermissions(int mode)
+{
+	char *str=malloc(sizeof(char)*10);
+	strcpy(str,"----------");
+	if((mode & 0170000) == 0010000)
+		str[0]='p';
+	else if((mode & 0170000) == 0020000)
+		str[0]='c';
+	else if((mode & 0170000) == 0040000)
+		str[0]='d';
+	else if((mode & 0170000) == 0060000)
+		str[0]='b';
+	else if((mode & 0170000) == 0100000)
+		str[0]='-';
+	else if((mode & 0170000) == 0120000)
+		str[0]='l';
+	else if((mode & 0170000) == 0140000)
+		str[0]='s';
+	else 
+		str[0]='~';		//unknown
+	if((mode & 0000400) == 0000400)
+		str[1]='r';
+	if((mode & 0000200) == 0000200)
+		str[2]='w';
+	if((mode & 0000100) == 0000100)
+		str[3]='x';
+	if((mode & 0000040) == 0000040)
+		str[4]='r';
+	if((mode & 0000020) == 0000020)
+		str[5]='w';
+	if((mode & 0000010) == 0000010)
+		str[6]='x';
+	if((mode & 0000004) == 0000004)
+		str[7]='r';
+	if((mode & 0000002) == 0000002)
+		str[8]='w';
+	if((mode & 0000001) == 0000001)
+		str[9]='x';
+	if((mode & 0004000) == 0004000)
+		str[3]='s';
+	if((mode & 0002000) == 0002000)
+		str[6]='s';
+	if((mode & 0001000) == 0001000)
+		str[9]='t';	
+	return str;
 }
