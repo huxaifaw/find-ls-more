567ee5079e2f7c676464f11d1a50ec638d52a6a7
Huzaifa Waseem <Huzaifa Waseem>
2019-03-24 23:31:48 +0500

    find command v1 (Searches file only in pwd :( )

--- myfind.c
+++ myfind.c
@@ -1,38 +1,45 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 
-extern int errno = 0;
+//extern int errno = 0;
 void myfind(char*, char*, char*);
 int main(int argc, char* argv[]) {
 	if(argc <= 3 || argc > 4) {
 		printf("Incomplete number of arguments\n");
-		return 1;	
+		exit(1);	
 	}	
-	else {
-		myfind(argv[1], argv[2], argv[3]);	
-	}
+	myfind(argv[1], argv[2], argv[3]);
 	return 0;	
 }
 
 void myfind(char* dir, char* type, char* name)
 {
-    	struct dirent * entry;
-	DIR * dp = opendir(dir);
+	//printf("%s\n", dir);
+    	DIR * dp = opendir(dir);
    	if (dp == NULL){
-      		fprintf(stderr, "Cannot open directory:%s\n", dir);
+      		printf("Cannot open directory:%s\n", dir);
       		return;
    	}
-	errno = 0;
+	struct dirent* entry;
+	//errno = 0;
 	while((entry = readdir(dp)) != NULL) {
-		if(entry->d_type == DT_DIR)
+		const char* dirName = dir;
+		chdir(dirName);
+		if(entry->d_type == DT_DIR && entry->d_name[0] != '.' )
 		{
 			char path[1024];
-			
-		}	
+			strcpy(path, dir);
+			strcat(path,"/");
+			strcat(path, entry->d_name);
+			myfind(path, type, name);
+		}
+		if(!strcmp(entry->d_name, name))
+			printf("%s/%s\n", dir, entry->d_name);	
 	}
-	closedir(dir);
+//	closedir(dp);
 }
